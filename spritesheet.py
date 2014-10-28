f35980c9590ee873f7c62a372a2f8e1efe21ac47
Chris Statzer <chris.statzer@gmail.com>
2014-10-28 11:59:14 +0800

    Cleanup of lesson 6 and spritesheet

--- spritesheet.py
+++ spritesheet.py
@@ -25,28 +25,3 @@ class SpriteSheet(object):
         created_sprite.image.blit(self._sheet_surface, (0,0,0,0), sprite_area)
         return created_sprite
 
-
-if __name__ == '__main__':
-    from pygame.locals import *
-    pygame.init()
-    ss = SpriteSheet('bling.sht')
-    sp = ss.new_sprite('ship')
-    sp.rect.x = 100
-    sp.rect.y = 100
-    sg = pygame.sprite.Group()
-    sg.add(sp, ss.new_sprite('star'))
-    screen = pygame.display.set_mode((700, 800))
-    game_running = True
-    while game_running:
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    game_running = False
-        screen.fill((0, 0, 0))
-        sg.draw(screen)
-        pygame.display.flip()
-
-    print sg, sp
-
-
-
